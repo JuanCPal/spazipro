98196f60d683dbbd411cb6507422363a2b91f258
JuanCPal <alone1110es@gmail.com>
2024-12-10 07:55:22 -0500

    Add files via upload

---
+++ portafolio.php
@@ -0,0 +1,163 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include ('../OnePage/templates/headersinsession.php') ?>
+<main class="main">
+
+<a href="https://wa.me/573125858719?text=Me%20gustaría%20saber%20el%20precio%20del%20de%20el%20mueble" class="whatsapp" target="_blank"> <i class="bi bi-whatsapp"></i></a>
+
+<!-- Page Title -->
+<div class="page-title accent-background">
+  <div class="container">
+    <h1>Portafolio de proyectos</h1>
+    <nav class="breadcrumbs">
+      <ol>
+        <li><a href="../OnePage/index.html">Inicio</a></li>
+        <li class="current">Portafolio</li>
+      </ol>
+    </nav>
+  </div>
+</div><!-- End Page Title -->
+
+<section id="portfolio" class="portfolio section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>Portfolio</h2>
+  <p>Muebles que Definen Espacios; Echa un Vistazo a Nuestro Trabajo</p>
+</div><!-- End Section Title -->
+
+<div class="container">
+
+  <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+
+    <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
+      <li data-filter="*" class="filter-active">Todo</li>
+    </ul><!-- End Portfolio Filters -->
+
+    <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
+        <img src="assets/img/portfolio/ce/2697458c915e3e04a1256839b2cc68e0.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <h4>Cocina Integral</h4>
+          <a href="assets/img/portfolio/ce/d801cf9499a9f7893351be3c080b3281.jpg" title="App 1"
+            data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <h4>Cocina Integral</h4>
+          <a href="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" title="Product 1"
+            data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-branding">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/a184380932719af87cc99a9a35dae425.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+
+          <a href="assets/img/portfolio/ce/2697458c915e3e04a1256839b2cc68e0.jpg" title="Branding 1"
+            data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/a184380932719af87cc99a9a35dae425.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <h4>Cocina Integral</h4>
+          <a href="../OnePage/assets/img/portfolio/Cocinas/a184380932719af87cc99a9a35dae425.jpg" title="App 2"
+            data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <h4>Cocina Integral</h4>
+          <a href="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" title="Product 2"
+            data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-branding">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+          <a href="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" title="Branding 2"
+            data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
+        <img src="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+
+          <a href="../OnePage/assets/img/portfolio/Cocinas/071a0b699569bb1ab13fbba77f5a015b.jpg" title="App 3"
+            data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
+        <img src="../OnePage/assets/img/portfolio/ce/52762cb06020f4ac92d40caaae0eaaef.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+    
+          <a href="../OnePage/assets/img/portfolio/ce/52762cb06020f4ac92d40caaae0eaaef.jpg" title="Product 3"
+            data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+      <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-branding">
+        <img src="../OnePage/assets/img/portfolio/Puertas/62c5f1d65826bceb97a38d09ff80e53e.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+     
+          <a href="../OnePage/assets/img/portfolio/Puertas/62c5f1d65826bceb97a38d09ff80e53e.jpg" title="Branding 2"
+            data-gallery="portfolio-gallery-branding" class="glightbox preview-link"><i
+              class="bi bi-zoom-in"></i></a>
+          <a href="portfolio-details.html" title="More Details" class="details-link"><i
+              class="bi bi-link-45deg"></i></a>
+        </div>
+      </div><!-- End Portfolio Item -->
+
+    </div><!-- End Portfolio Container -->
+
+
+  </div>
+</div>
+
+</section><!-- /Portfolio Section -->
+
+
+
+
+<?php include ('../OnePage/templates/footer.php') ?>
+</body>
+</html>
---
+++ servicios.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include ('../OnePage/templates/headersinsession.php') ?>
+<main class="main">
+
+<a href="https://wa.me/573125858719?text=Me%20gustaría%20saber%20el%20precio%20del%20de%20el%20mueble" class="whatsapp" target="_blank"> <i class="bi bi-whatsapp"></i></a>
+
+<!-- Page Title -->
+<div class="page-title accent-background">
+  <div class="container">
+    <h1>Servicios</h1>
+    <nav class="breadcrumbs">
+      <ol>
+        <li><a href="../OnePage/index.html">Inicio</a></li>
+        <li class="current">Servicios</li>
+      </ol>
+    </nav>
+  </div>
+</div><!-- End Page Title -->
+        <section id="services" class="services section light-background">
+
+          <!-- Section Title -->
+          <div class="container section-title" data-aos="fade-up">
+            <h2>Servicios</h2>
+            <p>En <b>SPAZI INNOVATIVO</b>, nos enorgullecemos de ofrecer soluciones personalizadas para transformar tus espacios. Nuestros servicios están diseñados para satisfacer las necesidades y deseos de nuestros clientes, asegurando que cada proyecto sea único y refleje tu estilo personal.</p>
+          </div><!-- End Section Title -->
+    
+          <div class="container">
+    
+            <div class="row gy-4">
+
+
+              <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+                <div class="service-item item-red position-relative">
+                  <div class="icon">
+                    <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,503.46388370962813C374.79870501325706,506.71871716319447,464.8034551963731,527.1746412648533,510.4981551193396,467.86667711651364C555.9287308511215,408.9015244558933,512.6030010748507,327.5744911775523,490.211057578863,256.5855673507754C471.097692560561,195.9906835881958,447.69079081568157,138.11976852964426,395.19560036434837,102.3242989838813C329.3053358748298,57.3949838291264,248.02791733380457,8.279543830951368,175.87071277845988,42.242879143198664C103.41431057327972,76.34704239035025,93.79494320519305,170.9812938413882,81.28167332365135,250.07896920659033C70.17666984294237,320.27484674793965,64.84698225790005,396.69656628748305,111.28512138212992,450.4950937839243C156.20124167950087,502.5303643271138,231.32542653798444,500.4755392045468,300,503.46388370962813"></path>
+                    </svg>
+                    <i class="bi bi-bounding-box-circles"></i>
+                  </div>
+                  <a href="service-details.html" class="stretched-link">
+                    <h3>Diseño personalizado de muebles</h3>
+                  </a>
+                  <p>Ofrecemos un servicio de diseño personalizado de muebles adaptandonos a las necesidades y preferencias de cada cliente</p>
+                  <a href="service-details.html" class="stretched-link"></a>
+                </div>
+              </div><!-- End Service Item -->
+
+    
+              <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="service-item item-teal position-relative">
+                  <div class="icon">
+                    <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,541.5067337569781C382.14930387511276,545.0595476570109,479.8736841581634,548.3450877840088,526.4010558755058,480.5488172755941C571.5218469581645,414.80211281144784,517.5187510058486,332.0715597781072,496.52539010469104,255.14436215662573C477.37192572678356,184.95920475031193,473.57363656557914,105.61284051026155,413.0603344069578,65.22779650032875C343.27470386102294,18.654635553484475,251.2091493199835,5.337323636656869,175.0934190732945,40.62881213300186C97.87086631185822,76.43348514350839,51.98124368387456,156.15599469081315,36.44837278890362,239.84606092416172C21.716077023791087,319.22268207091537,43.775223500013084,401.1760424656574,96.891909868211,461.97329694683043C147.22146801428983,519.5804099606455,223.5754009179313,538.201503339737,300,541.5067337569781"></path>
+                    </svg>
+                    <i class="bi bi-easel"></i>
+                  </div>
+                  <a href="service-details.html" class="stretched-link">
+                    <h3>Fabricacion y montaje profesional</h3>
+                  </a>
+                  <p>Nos encargamos de la fabricacion e instalacion de cada mueble, utilizando materiales de alta calidad y tecnicas presisas para garantizar la durabilidad y estetica</p>
+                </div>
+              </div><!-- End Service Item -->
+     
+    
+              <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+                <div class="service-item item-pink position-relative">
+                  <div class="icon">
+                    <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,566.797414625762C385.7384707136149,576.1784315230908,478.7894351017131,552.8928747891023,531.9192734346935,484.94944893311C584.6109503024035,417.5663521118492,582.489472248146,322.67544863468447,553.9536738515405,242.03673114598146C529.1557734026468,171.96086150256528,465.24506316201064,127.66468636344209,395.9583748389544,100.7403814666027C334.2173773831606,76.7482773500951,269.4350130405921,84.62216499799875,207.1952322260088,107.2889140133804C132.92018162631612,134.33871894543012,41.79353780512637,160.00259165414826,22.644507872594943,236.69541883565114C3.319112789854554,314.0945973066697,72.72355303640163,379.243833228382,124.04198916343866,440.3218312028393C172.9286146004772,498.5055451809895,224.45579914871206,558.5317968840102,300,566.797414625762"></path>
+                    </svg>
+                    <i class="bi bi-chat-square-text"></i>
+                  </div>
+                  <a href="service-details.html" class="stretched-link">
+                    <h3>Cotizacion y asesoria gratuita</h3>
+                  </a>
+                  <p>Realizamos una consulta de los requerimientos y preparamos una cotizacion detallada. Ademas, asesoramos a nuestros clientes en materiales, estilos y acabados</p>
+                  <a href="service-details.html" class="stretched-link"></a>
+                </div>
+              </div><!-- End Service Item -->
+    
+            </div>
+    
+          </div>
+    
+        </section><!-- /Services Section -->
+
+        <section id="about" class="about section">
+
+          <!-- Section Title -->
+          <div class="container section-title" data-aos="fade-up">
+            <h2>Sobre nosotros<br></h2>
+            <p>En <b>Spazi Innovativo</b>, nos dedicamos a transformar tus ideas en muebles funcionales y estéticamente excepcionales. Desde nuestras raíces, hemos trabajado incansablemente para ofrecer soluciones de diseño únicas, adaptadas a las necesidades de cada cliente. Nos especializamos en la fabricación de muebles integrales como <b>cocinas, closets, muebles de baño y centros de entretenimiento</b>, asegurando siempre la más alta <b>calidad y diseño.</b></p>
+          </div><!-- End Section Title -->
+    
+          <div class="container">
+    
+            <div class="row gy-4">
+    
+              <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+                <ul>
+                  <li><i class="bi bi-check2-circle"></i> <span>Trabajamos con materiales premium como Madecor, Madeplac y Madefondo para garantizar muebles duraderos y funcionales.</span></li>
+                  <li><i class="bi bi-check2-circle"></i> <span>Nuestro equipo se encarga de la instalación para que disfrutes de tus muebles sin preocupaciones.</span></li>
+                  <li><i class="bi bi-check2-circle"></i> <span>Nos importa tu satisfacción. Estamos aquí para responder a tus preguntas y hacer realidad tus ideas.</span></li>
+                </ul>
+              </div>
+    
+              <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                <p>
+                Nos mantenemos a la vanguardia en tendencias de diseño, buscando siempre la innovación. Desde el uso de materiales sostenibles hasta la incorporación de nuevas tecnologías en la fabricación, estamos comprometidos en brindarte soluciones modernas y funcionales.
+                </p>
+              </div>
+    
+            </div>
+    
+          </div>
+    
+        </section>
+
+      </main>
+      <?php include ('../OnePage/templates/footer.php') ?>
+</body>
+</html>
